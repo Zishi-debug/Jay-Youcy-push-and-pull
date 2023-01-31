aac254005139d851c3c7dc069276e0775b7367a3
vudieuhuong <dieuhuongvu31@gmail.com>
2023-01-31 14:07:03 +0000

    Part 2-31.01-Jay

---
+++ javascript/part2.js
@@ -0,0 +1,39 @@
+let sky, body, lotus, lotus2;
+let x, y, x_, y_;
+function preload() {
+  sky = loadImage('/source/lotus/sky.jpeg');
+  body = loadImage('/source/lotus/body.png');
+  lotus = loadImage('/source/lotus/lotus.png');
+  lotus2 = loadImage('/source/lotus/lotus2.png');
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("sketch-container");
+  x = 0;
+  y = 0;
+  x_ = windowWidth/2;
+  y_ = windowHeight;
+}
+
+function draw() {
+  background(255);
+  
+  fill(0);
+  rect(0, windowHeight/2, windowWidth, windowHeight/2);
+  
+  // Draw images
+  image(sky, 0, 0, windowWidth, windowHeight/2);
+  image(lotus, x_, y_, 400, 400);
+  image(lotus2, windowWidth - 200, 300, 200, 400);
+  image(body, x, y, 300, 500);
+  x ++;
+  y +=2;
+  
+  if(y > windowHeight) {
+    y_ --;
+    }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,83 +1,78 @@
 let hand= [];
-let handPlay;
-let handDraw = false;
-
 let heart=[];
+let handPlay;
 let r,g,b;
 let poem;
+let handDraw = true;
 
 function preload(){
-  hand[0]= loadImage('javascript/hand/hand1副本.png');
-  hand[1]= loadImage('javascript/hand/hand2副本.png');
-  hand[2]= loadImage('javascript/hand/hand3副本.png');
-  hand[3]= loadImage('javascript/hand/hand4副本.png');
-  hand[4]= loadImage('javascript/hand/hand5副本.png')
+  hand[0]= loadImage('source/hand/hand1.png');
+  hand[1]= loadImage('source/hand/hand2.png');
+  hand[2]= loadImage('source/hand/hand3.png');
+  hand[3]= loadImage('source/hand/hand4.png');
+  hand[4]= loadImage('source/hand/hand5.png');
 }
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    imageMode(CENTER);
-    handPlay=0;
-    r=0
-    strokeWeight(1);
-poem = 'Tell me it was for the hunger & nothing less. For hunger is to give the body what it knowsit cannot keep. That this amber light whittled down by another waris all that pins my hand to your chest.'
- 
-        heart[0]= createVector(random(width/2),random(height/2));
-        heart[1]= createVector(random(width/2,width),random(height/2));
-        heart[3]= createVector(random(width/2),random(height/2,height));
-        heart[2]= createVector(random(width/2,width),random(height/2,height));
-
-    
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  handPlay=0;
+  r=0
+  strokeWeight(1);
+  poem = 'Tell me it was for the hunger & nothing less. For hunger is to give the body what it knowsit cannot keep. That this amber light whittled down by another waris all that pins my hand to your chest.'
+  heart[0]= createVector(random(width/2),random(height/2));
+  heart[1]= createVector(random(width/2,width),random(height/2));
+  heart[3]= createVector(random(width/2),random(height/2,height));
+  heart[2]= createVector(random(width/2,width),random(height/2,height));  
   }
   
-  function draw() {
-    background(220);
-fill(r,52,255);
-noStroke();
+function draw() {
+  background(220);
+  fill(r,52,255);
+  noStroke();
 
 //draw heart here;
-beginShape();
-curveVertex(heart[0].x, heart[0].y);
-for(let i=0; i<4;i++){
+  beginShape();
+  curveVertex(heart[0].x, heart[0].y);
+  for(let i=0; i<4;i++){
     curveVertex(heart[i].x, heart[i].y);
-}
-curveVertex(heart[3].x, heart[3].y);
-endShape(CLOSE);
+  }
+  curveVertex(heart[3].x, heart[3].y);
+  endShape(CLOSE);
 
 //text here
-fill(0,52,49);
-push();
-translate((heart[0].x+heart[1].x)/2,(heart[0].x+heart[3].x)/2);
-text(poem,0,0,200);
-pop();
-
+  fill(0,52,49);
+  push();
+  translate((heart[0].x+heart[1].x)/2,(heart[0].x+heart[3].x)/2);
+  text(poem,0,0,200);
+  pop();
 
 //draw hand here
-if(handDraw){
+  if(handDraw){
     push();
     translate(mouseX,mouseY);
     scale(0.5,0.5);
     image(hand[handPlay],0,0);
     pop();
+  }
 }
-  }
 
-  function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
-  function mouseDragged(){
-   handDraw = true;
-   if(frameCount%10 ===0){
+function mouseDragged(){
+  handDraw = true;
+  if(frameCount%10 ===0){
     if(handPlay<4){
-        handPlay++;
-       if(r<222){
+      handPlay++;
+      if(r<222){
         r+=10;
-       }else{
+        }else{
         r--;
        }
     
     }else{
-        handPlay=0;
+      handPlay=0;
     }   
    }
   }
