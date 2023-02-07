98cc72f86e8dab7889ce881f4044d06a9517c5e8
Zishi-debug <tuzishizhenkeai@gmail.com>
2023-02-07 17:56:31 +0000

    upload the part 3_Youcy_07_02
    -click on the screen to interact.
    -changed some linkstyle in css.
    - I tried to use loadSound but it said it's undefined function. I added <script src="path/to/p5.sound.js"></script>  into the header of index.html , but it doesn’t work.

---
+++ javascript/part3.js
@@ -0,0 +1,40 @@
+//I tried to use loadSound but it said it's undefined fucntion.
+// I add <script src="path/to/p5.sound.js"></script> in the header of index.html. But it doesn't work.
+let rose, chainsaw;
+let moveX, moveY;
+let shake;
+function preload(){
+    rose = loadImage('../../source/rose/rose.png');
+    //chainsaw = loadSound('../../source/rose/chainsaw.wav');
+    
+}
+function setup(){
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent("sketch-container");
+    imageMode(CENTER);
+
+    moveX = 0;
+    moveY = 0;
+    shake = false;
+}
+function draw(){
+    background(255);
+    push();
+    translate(windowWidth/2+ moveX, windowHeight/2+ moveY);
+    scale(0.1,0.1);
+    image(rose,0,0);
+    pop();
+    if(shake){
+        moveX =random(-5,5);
+        moveY =random(-5,5);
+        //chainsaw.play();
+    }
+}
+function mousePressed(){
+   shake= true;
+}
+
+function mouseReleased(){
+    shake= false;
+ }
+ 
